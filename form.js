f99bc91add7915eeed01f551cfc7fca87809957f
CarlosMarv <carlosmmarveggio@gmail.com>
2023-06-11 20:46:43 -0300

    Corrección de formulario

--- form.js
+++ form.js
@@ -1,83 +1,69 @@
-const formulario = document.getElementById('form-contacto');
-const inputs = document.querySelectorAll('#form-contacto input');
+let primero = document.querySelector("form input");
+primero.focus();
+let reg_nombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+let reg_correo = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
+let reg_telefono = /^\d{7,14}$/;
 
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+function validar() {
+	
+  // variables para comprobar
+  let f_nombre = document.querySelector("#f_nombre");
+  f_nombre.classList.remove("error");
+	document.querySelector('#msj-nombre').classList.add('mensaje-error-inactivo')
+	document.querySelector('#msj-nombre').classList.remove('mensaje-error-activo')	
+  let f_correo = document.querySelector("#f_correo");
+  f_correo.classList.remove("error");
+	document.querySelector('#msj-correo').classList.add('mensaje-error-inactivo')
+	document.querySelector('#msj-correo').classList.remove('mensaje-error-activo')	
+  let f_telefono = document.querySelector("#f_telefono");
+  f_telefono.classList.remove("error");
+	document.querySelector('#msj-telefono').classList.add('mensaje-error-inactivo')
+	document.querySelector('#msj-telefono').classList.remove('mensaje-error-activo')
+	let f_motivo = document.querySelector("#f_motivo");
+	document.querySelector('#msj-motivo').classList.add('mensaje-error-inactivo')
+	document.querySelector('#msj-motivo').classList.remove('mensaje-error-activo')
+	
+	let lta_mensajes = document.querySelector("#mensajes");
+  // bandera
+  let hubo_error = false;
+  // comprobaciones
+  if (f_nombre.value == "" || !reg_nombre.test(f_nombre.value)) {
+    f_nombre.classList.add("error");
+		document.querySelector('#msj-nombre').classList.remove('mensaje-error-inactivo')
+		document.querySelector('#msj-nombre').classList.add('mensaje-error-activo')
+    hubo_error = true;
+  }
+
+	if (f_correo.value == "" || !reg_correo.test(f_correo.value)){
+		f_correo.classList.add("error");
+		document.querySelector('#msj-correo').classList.remove('mensaje-error-inactivo')
+		document.querySelector('#msj-correo').classList.add('mensaje-error-activo')
+    hubo_error = true;
+  }
+
+	if (f_telefono.value == "" || !reg_telefono.test(f_telefono.value)){
+		f_telefono.classList.add("error");
+		document.querySelector('#msj-telefono').classList.remove('mensaje-error-inactivo')
+		document.querySelector('#msj-telefono').classList.add('mensaje-error-activo')
+    hubo_error = true;
+	}
+
+	if (f_motivo.value == "value="){
+		document.querySelector('#msj-motivo').classList.remove('mensaje-error-inactivo')
+		document.querySelector('#msj-motivo').classList.add('mensaje-error-activo')
+    hubo_error = true;
+	}
+
+	if (hubo_error){
+		return false;
+	}else{
+		let p = document.createElement("p");
+		p.innerHTML =
+		"Se envía: Nombre: "+f_nombre.value+", "+"Correo: "+f_correo.value+", "+"Teléfono: "+f_telefono.value+", Motivo: "+f_motivo.value
+		lta_mensajes.appendChild(p);
+		document.forms['f_contacto'].reset();
+		let primero = document.querySelector("form input");
+    primero.focus();
+	}
+	return false;
 }
-
-const campos = {
-	nombre: false,
-	correo: false,
-	telefono: false
-}
-
-const validarFormulario = (e) => {
-    document.getElementById('faltan-campos').classList.remove('mensaje-error')
-    document.getElementById('faltan-campos').classList.add('mensaje-error-inactivo')
-	switch (e.target.name) {
-		case "nombre":
-            if (expresiones.nombre.test(e.target.value)){
-                document.getElementById('nombre').classList.remove('error')
-                document.querySelector('#msj-nombre').classList.remove('mensaje-error')
-                document.querySelector('#msj-nombre').classList.add('mensaje-error-inactivo')
-                campos.nombre = true;
-            }else{
-                document.getElementById('nombre').classList.add('error')
-                document.querySelector('#msj-nombre').classList.add('mensaje-error')
-                document.querySelector('#msj-nombre').classList.remove('mensaje-error-inactivo')
-                campos.nombre = false;
-            }
-		break;
-		case "correo":
-			if (expresiones.correo.test(e.target.value)){
-                document.getElementById('correo').classList.remove('error')
-                document.querySelector('#msj-correo').classList.remove('mensaje-error')
-                document.querySelector('#msj-correo').classList.add('mensaje-error-inactivo')                
-                campos.correo = true;
-            }else{
-                document.getElementById('correo').classList.add('error')
-                document.querySelector('#msj-correo').classList.add('mensaje-error')
-                document.querySelector('#msj-correo').classList.remove('mensaje-error-inactivo')
-                campos.correo = false;
-            }
-		break;
-		case "telefono":
-			if (expresiones.telefono.test(e.target.value)){
-                document.getElementById('telefono').classList.remove('error')
-                document.querySelector('#msj-telefono').classList.remove('mensaje-error')
-                document.querySelector('#msj-telefono').classList.add('mensaje-error-inactivo')   
-                campos.telefono = true;
-            }else{
-                document.getElementById('telefono').classList.add('error')
-                document.querySelector('#msj-telefono').classList.add('mensaje-error')
-                document.querySelector('#msj-telefono').classList.remove('mensaje-error-inactivo')
-                campos.telefono = false;
-            }
-		break;
-    }
-}
-
-inputs.forEach((input) => {
-	input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', (e)=> {
-    e.preventDefault();
-
-    if(campos.nombre && campos.correo && campos.telefono){
-        
-        alert('Se envían los datos:\n'+
-        'nombre: '+formulario.nombre.value + '\n'+
-        'correo: '+formulario.correo.value +'\n'+
-        'telefono: '+formulario.telefono.value+'\n'+
-        'motivo de contacto: '+formulario.motivo.value)
-        formulario.reset();
-    }else{
-        document.getElementById('faltan-campos').classList.add('mensaje-error')
-        document.getElementById('faltan-campos').classList.remove('mensaje-error-inactivo')
-    }
-    
-});
